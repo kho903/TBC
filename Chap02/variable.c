aca11e80604592c20931721bdabbaf6fc134cba9
kho903 <gmldnr2222@naver.com>
2021-02-10 14:24:27 +0900

    변수 선언 방법

---
+++ Chap02/variable.c
@@ -0,0 +1,24 @@
+int main()
+{
+    // 변수 이름은 마음대로
+    // 알파벳 대소문자, _, 숫자(첫글자 X)
+    // 특수문자 X, +, - 등 X, 예약어 X
+    int num_apples;
+    int my_love;
+    int house;
+    int cracker;
+    int a;
+    int A;
+    // int a; 중복 X
+    // int tax rate; 빈칸 비허용
+
+    // int x; // declaration
+    // int y;
+    // int z;
+    int x = 1, y = 2, z; // 같은 자료형일 때, 가능
+
+    // x = 1; // assignment
+    // y = 2;
+    z = x + y;
+    return 0;
+}
