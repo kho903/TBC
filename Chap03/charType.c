097f363d188e58b926f9fda4a20a18dd8f74d54b
kho903 <gmldnr2222@naver.com>
2021-02-12 18:02:36 +0900

    문자형

---
+++ Chap03/charType.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    char c = 'A';
+    char d = 65; // d = 'A';
+    char e = '*';
+
+    printf("%c %hhd\n", c, c); // A 65
+    printf("%c %hhd\n", d, d); // A 65
+    printf("%c %hhd\n", e, e); // * 42
+
+    printf("%c \n", c + 1); // B
+    */
+
+    /*
+    char a = '\a';
+    printf("%c", a);
+    printf("\07");
+    printf("\x7");
+    */
+
+    // printf("\x23"); // #
+
+    /*
+    float salary;
+    // printf("$______");
+    printf("$______\b\b\b\b\b\b");
+    scanf("%f", &salary);
+    */
+
+    // printf("AB\tCDEF\n"); // AB      CDEF
+    // printf("ABC\tDEF\n"); // ABC     DEF
+
+    printf("\\ \'HA+\' \"Hello\" \?\n");
+
+    return 0;
+}
