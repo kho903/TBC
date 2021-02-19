8365127b9bf430ecf03fb721c24aded951a13aab
kho903 <gmldnr2222@naver.com>
2021-02-19 21:24:57 +0900

    분기문 if

---
+++ Chap07/if.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+
+    printf("Input a positive integer : ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0)
+        printf("Even");
+    else // if (number % 2 != 0)
+        printf("Odd");
+
+    return 0;
+}
