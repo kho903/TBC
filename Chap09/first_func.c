9bc44068db6de0342bea76aa161b70d0c84fabec
kho903 <gmldnr2222@naver.com>
2021-02-23 10:38:05 +0900

    함수의 프로토타입

--- Chap09/first_func.c
+++ Chap09/first_func.c
@@ -6,7 +6,23 @@
 #define NAME "JIHUN KIM"
 #define ADDRESS "SUWON, KOREA"
 
-void print_multiple_chars(char c, int n_stars, bool print_newline)
+// prototype
+void print_multiple_chars(char c, int n_stars, bool print_newline);
+void print_centered_str(char str[]);
+
+int main()
+{
+    print_multiple_chars('*', WIDTH, true); // argument
+    print_centered_str(NAME);
+    print_centered_str(ADDRESS);
+    print_centered_str("I LOVE YOU");
+
+    print_multiple_chars('*', WIDTH, false);
+
+    return 0;
+}
+
+void print_multiple_chars(char c, int n_stars, bool print_newline) // formal (argument / parameter)
 {
     for (int i = 0; i < n_stars; ++i)
         printf("%c", c);
@@ -21,16 +37,4 @@ void print_centered_str(char str[])
     n_blanks = (WIDTH - strlen(str)) / 2;
     print_multiple_chars(' ', n_blanks, false);
     printf("%s\n", str);
-}
-
-int main()
-{
-    print_multiple_chars('*', WIDTH, true);
-    print_centered_str(NAME);
-    print_centered_str(ADDRESS);
-    print_centered_str("I LOVE YOU");
-
-    print_multiple_chars('*', WIDTH, false);
-
-    return 0;
 }
