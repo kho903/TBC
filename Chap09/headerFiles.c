4d1f9d9c3f0c7c184c92af2719d3620b00cb80af
kho903 <gmldnr2222@naver.com>
2021-02-23 14:42:42 +0900

    헤더 파일 활용

---
+++ Chap09/headerFiles.c
@@ -0,0 +1,14 @@
+#include "headerFiles_header.h"
+
+int main()
+{
+    print_hello();
+    print_hi();
+    print_hello();
+    print_hi();
+    print_str("No one lives forever");
+    print_str("Valar morghulis");
+
+    printf("Hello!");
+    return 0;
+}
