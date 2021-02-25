3cfa45f0e2665b8623cfc7111235f35f5a870837
kho903 <gmldnr2222@naver.com>
2021-02-26 01:38:23 +0900

    NULL 포인터와 런타임 에러

---
+++ Chap09/Pointer/nullPointer_runtimeE.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    int a = 1234;
+    printf("%p\n", &a);
+    printf("%d\n", a);
+    */
+
+    /*
+    int *ptr = 1234; // error
+
+    printf("%p\n", ptr);
+    printf("%d\n", *ptr);
+    */
+
+    int *safer_ptr = NULL; // = NULL;
+    int a = 123;
+    // safer_ptr = &a;
+
+    int b;
+    scanf("%d", &b);
+
+    if (b % 2 == 0)
+        safer_ptr = &a;
+
+    if (safer_ptr != NULL)
+        printf("%p\n", safer_ptr);
+
+    if (safer_ptr != NULL)
+        printf("%d\n", *safer_ptr);
+
+    return 0;
+}
