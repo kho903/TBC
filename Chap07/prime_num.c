73cb5e7f96b2e3577306f0ff67165f3e27c3d8ce
kho903 <gmldnr2222@naver.com>
2021-02-27 16:03:30 +0900

    소수 판단 및 약수 출력 예제

---
+++ Chap07/prime_num.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    unsigned num, div;
+    bool isPrime; // flag, try bool type
+    scanf("%u", &num);
+
+    /*
+        TODO : Check if num is a prime number
+        ex) num is 4, try num % 2, num % 3
+        ex) num is 5, try num % 2, num % 3, num % 5
+    */
+
+    isPrime = true;
+    for (div = 2; (div * div) <= num; div++)
+    {
+        if (num % div == 0)
+        {
+            isPrime = false;
+            if (num == div * div)
+                printf("%u is divisible by %u\n", num, div);
+            else
+                printf("%u is divisible by %u and %u\n", num, div, num / div);
+        }
+    }
+
+    if (isPrime)
+        printf("%u is a prime number.\n", num);
+    else
+        printf("%u is not a prime number.\n", num);
+
+    return 0;
+}
