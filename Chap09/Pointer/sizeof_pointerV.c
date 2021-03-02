3986be3b07d09d6a478451ef09a81a274deaf2e0
kho903 <gmldnr2222@naver.com>
2021-03-02 18:02:18 +0900

    포인터 변수의 크기

---
+++ Chap09/Pointer/sizeof_pointerV.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    char a;
+    float b;
+    double c;
+    char *ptr_a = &a;
+    float *ptr_b = &b;
+    double *ptr_c = &c;
+
+    printf("%zd %zd %zd\n", sizeof(a), sizeof(b), sizeof(c));
+    printf("%zd %zd %zd\n", sizeof(ptr_a), sizeof(ptr_b), sizeof(ptr_c));
+    printf("%zd %zd %zd\n", sizeof(&a), sizeof(&b), sizeof(&c));
+    printf("%zd %zd %zd\n", sizeof(char *), sizeof(float *), sizeof(double *));
+}
